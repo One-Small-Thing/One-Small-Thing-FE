3cda5f3fbb8164c94cac937cfdda79e3d4c75b75
Cami Anderson <camidantas@icloud.com>
2022-08-27 13:19:13 -0400

    Add NewsStories component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,14 @@ import NavBar from '../NavBar/NavBar';
 import HotTopics from '../HotTopics/HotTopics';
 import Footer from '../Footer/Footer';
 import './App.css';
+import NewsStories from '../NewsStories/NewsStories';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <HotTopics />
+      <NewsStories />
       <Footer />
     </div>
   );
---
+++ src/components/NewsStories/NewsStories.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NewsStories = () => {
+    return (
+        <p>News</p>
+    )
+}
+
+export default NewsStories;
