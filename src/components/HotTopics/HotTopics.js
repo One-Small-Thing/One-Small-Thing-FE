c75aabb4a33cedab13cb88a95ae2306f12fa2a9d
Cami Anderson <camidantas@icloud.com>
2022-08-27 15:47:04 -0400

    Remove a tags

--- src/components/HotTopics/HotTopics.js
+++ src/components/HotTopics/HotTopics.js
@@ -9,15 +9,15 @@ const HotTopics = () => {
                 <p>When the world is at it's bleakest, sometimes it just needs brave souls who are willing to help.. Even if it's just one small thing.</p>
             </div>
             <div className='hot-topics__buttons'>
-                <a>Enviroment</a>
-                <a>Reproductive Rights</a>
-                <a>Education</a>
-                <a>LGBTQIA+</a>
-                <a>Domestic Violence</a>
-                <a>Free & Open Computing</a>
-                <a>Gun Control</a>
-                <a>All Topics</a>
-                <a>Surprise Me!</a>
+                <div className='topics'>Enviroment</div>
+                <div className='topics'>Reproductive Rights</div>
+                <div className='topics'>Education</div>
+                <div className='topics'>LGBTQIA+</div>
+                <div className='topics'>Domestic Violence</div>
+                <div className='topics'>Free & Open Computing</div>
+                <div className='topics'>Gun Control</div>
+                <div className='topics'>All Topics</div>
+                <div className='topics'>Surprise Me!</div>
             </div>
 
         </section>
