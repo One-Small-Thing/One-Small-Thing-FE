a38abfc89e11f45aa746f499930036a414c28224
Cami Anderson <camidantas@icloud.com>
2022-08-27 13:09:09 -0400

    Add NavBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <p>NavBar</p>
+    )
+}
+export default NavBar;
