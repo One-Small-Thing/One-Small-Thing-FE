7256b47eaa63f67056885679ea39e3b5d622b88c
Cami Anderson <camidantas@icloud.com>
2022-08-27 14:45:14 -0400

    Add basic css to the page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './NavBar.css';
 
+
 const NavBar = () => {
     return (
         <nav className='navbar'>
-            <p>One Small Thing</p>
+            <img className='navbar__img' src='https://cdn-icons-png.flaticon.com/512/1917/1917613.png'></img>
+            <h1>One Small Thing</h1>
+            <p>menu</p>
         </nav>
     )
 }
