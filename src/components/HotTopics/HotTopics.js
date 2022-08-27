13843872a269612a1a64b8ea7ce0e2899968ac9c
Cami Anderson <camidantas@icloud.com>
2022-08-27 15:19:50 -0400

    Add HotTopics basic styling

--- src/components/HotTopics/HotTopics.js
+++ src/components/HotTopics/HotTopics.js
@@ -4,7 +4,22 @@ import './HotTopics.css';
 const HotTopics = () => {
     return (
         <section className='hot-topics'>
-            <h1>HotTopics</h1>
+            <div className='hot-topics__desc'>
+                <h1>One Small Thing</h1>
+                <p>When the world is at it's bleakest, sometimes it just needs brave souls who are willing to help.. Even if it's just one small thing.</p>
+            </div>
+            <div className='hot-topics__buttons'>
+                <a>Enviroment</a>
+                <a>Reproductive Rights</a>
+                <a>Education</a>
+                <a>LGBTQIA+</a>
+                <a>Domestic Violence</a>
+                <a>Free & Open Computing</a>
+                <a>Gun Control</a>
+                <a>All Topics</a>
+                <a>Surprise Me!</a>
+            </div>
+
         </section>
     )
 }
