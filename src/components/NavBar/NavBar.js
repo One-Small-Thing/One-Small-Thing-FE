0b1fa529f9e2207c70e3e9d8f057a75418c2dc06
Cami Anderson <camidantas@icloud.com>
2022-08-28 11:50:28 -0400

    Add menu to NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,6 +10,19 @@ const NavBar = () => {
                 <h1 className='navbar__main--display'>One Small Thing</h1>
                 <div className='navbar__main--menu navbar__main--display'>
                     <input type='checkbox' className='navbar__main--menu--checkbox' id='navi-toggle'/>
+                    <label htmlFor='navi-toggle' className='navbar__main--menu--button'>
+                        <span className='navbar__main--menu--icon'>&nbsp;</span>
+                    </label>
+                    <div className='navbar__main--menu--background'>&nbsp;</div>
+                    <nav className='navbar__main--menu--nav'>
+                        <ul className='navbar__main--menu--list'>
+                            <li className='navbar__main--menu--item'><a href='#' className='navbar__main--menu--link'>About 1</a></li>
+                            <li className='navbar__main--menu--item'><a href='#' className='navbar__main--menu--link'>About 2</a></li>
+                            <li className='navbar__main--menu--item'><a href='#' className='navbar__main--menu--link'>About 3</a></li>
+                            <li className='navbar__main--menu--item'><a href='#' className='navbar__main--menu--link'>About 4</a></li>
+                            <li className='navbar__main--menu--item'><a href='#' className='navbar__main--menu--link'>About 5</a></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
