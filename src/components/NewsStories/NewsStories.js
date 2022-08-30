37382c6f55829df122ed31f140a78bd6eb8009e1
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-08-30 11:23:15 -0600

    Update link for postman mock server

--- src/components/NewsStories/NewsStories.js
+++ src/components/NewsStories/NewsStories.js
@@ -5,25 +5,37 @@ import { GET_NEWS_BY_TAG } from '../../queries';
 const NewsStories = ({topic}) => {
 
     const { loading, error, data } = useQuery(GET_NEWS_BY_TAG, {tags: topic})
-    
-      // const [ categoryNews, setCategoryNews ] = useState([])
-      // const [ currentTag, setCurrentTag ] = useState('')
-
-
-    if(loading) return "Loading..."
-    if(error) return `Error! ${error.message}`
-    console.log(data)
+  //   const news = data.news.map((story => {
+  //     return (
+  //       <section className="story-section" src={story.img}>
+  //       <div className="tag-box">
+  //         <p className="tags">{story.tags}</p>
+  //       </div>
+  //       <div className="action-box">
+  //         <p className="action">TAKE ACTION</p>
+  //       </div>
+  //       <div className="title-box">
+  //       <h2 className="news-story-title">{story.title}</h2>
+  //         <p className="author-name">by {story.author}</p>
+  //       </div>
+  //     </section>
+  //   )
+  // }))
+  // const [ categoryNews, setCategoryNews ] = useState([])
+  // const [ currentTag, setCurrentTag ] = useState('')
+  
+  
+  // if(loading) return "Loading..."
+  // if(error) return `Error! ${error.message}`
+  // console.log(data)
+  console.log(data.news)
     
     return (
-      <section className="story-section" >
-        <div className="top-left-corner-box">
-          <p className="tags">TAG HERE</p>
-        </div>
-        <h2 className="news-story-title">News Article Title</h2>
-        <div className="bottom-left-corner-box">
-          <p className="author-name">Author Name</p>
-        </div>
-      </section>
+      <div>
+        HELLO
+ 
+      </div>
+   
     )
 }
 
