1eaac67b990384beb054dbb90739d6ee3b91fef0
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-09-01 23:35:16 -0600

    Add in default image for stories with img value of null

--- src/components/NewsStories/NewsStories.js
+++ src/components/NewsStories/NewsStories.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useQuery } from '@apollo/client';
 import { GET_NEWS_BY_HEADLINE } from '../../queries';
 import "./NewsStories.css"
+import peace from "../../images/peace.jpg"
 
 const NewsStories = () => {
   const { loading, error, data } = useQuery(GET_NEWS_BY_HEADLINE)
@@ -17,8 +18,9 @@ const NewsStories = () => {
     
     return (
       <div className="story">
-        <section key={story.title} className="story__section" 
-        style={{ backgroundImage:`url(${story.img})` }}>
+        {story.img === null ?
+        <section key={story.title} className="story__section" style={{ backgroundImage:`url(${peace})` }}
+                  alt="woman with making peace symbol with hand in air at a rally">
             <div className="story__section__tag-box">
               <p className="tags">EDUCATION {story.tags}</p>
               <p className="action">TAKE ACTION E</p>
@@ -29,6 +31,20 @@ const NewsStories = () => {
               <p className="date">{story.date}</p>
             </div>
           </section>
+          : 
+          <section key={story.title} className="story__section" style={{ backgroundImage:`url(${story.img})` }}
+                    alt="still image scene from corresponding news article">
+              <div className="story__section__tag-box">
+                <p className="tags">EDUCATION {story.tags}</p>
+                <p className="action">TAKE ACTION E</p>
+              </div>
+              <div className="story__section__title-box">
+                <h2 className="title">{story.title}</h2>
+                <p className="author-name">by Author Mc Authorson {story.author}</p>
+                <p className="date">{story.date}</p>
+              </div>
+          </section>
+        }
       </div>
     )
     })
