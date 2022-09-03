64e55e08895dffea25f6a6737c31646aca2b2b1e
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-09-03 15:02:57 -0600

    Begin modal options for take action button

--- src/components/NewsStories/NewsStories.js
+++ src/components/NewsStories/NewsStories.js
@@ -32,7 +32,7 @@ const NewsStories = (event) => {
                   alt="woman with making peace symbol with hand in air at a rally">
             <div className="story__section__tag-box">
               <p className="tags">{event.topic}</p>
-              <p className="action">TAKE ACTION</p>
+              <label htmlFor="mpo-modal-controller" className="btn">TAKE ACTION</label>
             </div>
             <div className="story__section__title-box">
               <h2 className="title">{story.title}</h2>
@@ -44,7 +44,7 @@ const NewsStories = (event) => {
                     alt="still image scene from corresponding news article">
               <div className="story__section__tag-box">
                 <p className="tags">{event.topic}</p>
-                <p className="action">TAKE ACTION</p>
+                <label htmlFor="mpo-modal-controller" className="btn">TAKE ACTION</label>
               </div>
               <div className="story__section__title-box">
                 <h2 className="title">{story.title}</h2>
@@ -53,6 +53,68 @@ const NewsStories = (event) => {
               </div>
           </section>
         }
+<div className="mpo-modal">
+  <input type="checkbox" id="mpo-modal-controller" className="mpo-modal-open" hidden/>
+  <div className="mpo-modal-wrap">
+    <label htmlFor="mpo-modal-controller" className="mpo-modal-overlay"></label>
+    <div className="mpo-modal-body">
+
+      <label htmlFor="mpo-modal-controller" className="mpo-modal-close">&times;</label>
+
+      <input type="radio" name="content-nav" id="modal-content-1" className="modal-radio" checked hidden/>
+      <input type="radio" name="content-nav" id="modal-content-2" className="modal-radio" hidden/>
+      <input type="radio" name="content-nav" id="modal-content-3" className="modal-radio" hidden/>
+
+      <div className="mpo-modal-slide content-1">
+        <div className="mpo-modal-content">
+          <h2>Example Modal - Slide 1</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+
+          <div className="mpo-modal-nav">
+            <label htmlFor="modal-content-2" className="next-slide">&#8250;&#8250;</label>
+          </div>
+        </div>
+      </div>
+
+      <div className="mpo-modal-slide content-2">
+        <div className="mpo-modal-content">
+          <h2>Example Modal - Slide 2</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+
+          <div className="mpo-modal-nav">
+            <label htmlFor="modal-content-1" className="prev-slide">&#8249;&#8249;</label>
+            <label htmlFor="modal-content-3" className="next-slide">&#8250;&#8250;</label>
+          </div>
+        </div>
+      </div>
+
+      <div className="mpo-modal-slide content-3">
+        <div className="mpo-modal-content">
+          <h2>Example Modal - Slide 3</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+
+          <div className="mpo-modal-nav">
+            <label htmlFor="modal-content-2" className="prev-slide">&#8249;&#8249;</label>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</div>
+
+
+
       </div>
     )
     })
