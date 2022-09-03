823b1e9fa30ec4ddb064a8bbe0d89b9262e245a0
Neil Linton <lintonbneil@gmail.com>
2022-09-03 14:26:30 -0700

    Fix error handling in News comp

--- src/components/NewsStories/NewsStoryCard.js
+++ src/components/NewsStories/NewsStoryCard.js
@@ -6,19 +6,22 @@ const NewsStoryCard = ({id, date, title, img, description, author, url}) => {
 
     const newBackground = img || peace
 
-    return (
-        <section className="story__section" style={{backgroundImage:`url(${newBackground})`}}
-        alt="still image scene from corresponding news article">
-            <div className="story__section__tag-box">
-                <p className="tags">EDUCATION</p>
-                <p className="action">TAKE ACTION</p>
-            </div>
-            <div className="story__section__title-box">
-                <h2 className="title">{title}</h2>
-                <p className="author-name">by Author Mc Authorson</p>
-                <p className="date">{date}</p>
-            </div>
-        </section>
+    return (       
+            <section className="story__section" style={{backgroundImage:`url(${newBackground})`}}
+            alt="still image scene from corresponding news article">
+                <div className="story__section__tag-box">
+                    <p className="tags">EDUCATION</p>
+                    <p className="action">TAKE ACTION</p>
+                </div>
+                <div className="story__section__title-box">
+                    <h2 className="title">{title}</h2>
+                    <p className="author-name">by Author Mc Authorson</p>
+                    <p className="date">{date}</p>
+                </div>
+                <Link className="Link__Article" to={{pathname: "/Article", state: url}}>
+                    <button>Article</button>
+                </Link>
+            </section>
     )
 }
 
