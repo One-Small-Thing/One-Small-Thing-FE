619b0e1cfe20418cbc1e234ec926f0679db54e82
Neil Linton <lintonbneil@gmail.com>
2022-09-03 12:51:35 -0700

    Add reusuable component for generating cards

--- src/components/NewsStories/NewsStories.js
+++ src/components/NewsStories/NewsStories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import NewsStoryCard from './NewsStories/NewStoryCard';
 import { useQuery } from '@apollo/client';
 import { GET_NEWS_BY_HEADLINE } from '../../queries';
 import "./NewsStories.css"
@@ -9,15 +10,13 @@ const NewsStories = () => {
 
   if(loading) return "Loading..."
   if(error) return `Error! ${error.message}`
-    const headlinesArr = data.headlines
-    console.log(headlinesArr)
-    
-  if(headlinesArr){
-    const news = headlinesArr.map(story => {
-
+  if(data){
+    const storyArr = data.headlines;
+    const news = storyArry.map(story => {
+      
     return (
       <div className="story">
-
+        <NewsStoryCard />
       </div>
     )
     })
@@ -35,3 +34,15 @@ export default NewsStories;
         
    
         
+// <section key={story.title} className="story__section" style={{ backgroundImage:`url(${story.img})` }}
+// alt="still image scene from corresponding news article">
+// <div className="story__section__tag-box">
+// <p className="tags">EDUCATION {story.tags}</p>
+// <p className="action">TAKE ACTION</p>
+// </div>
+// <div className="story__section__title-box">
+// <h2 className="title">{story.title}</h2>
+// <p className="author-name">by Author Mc Authorson {story.author}</p>
+// <p className="date">{story.date}</p>
+// </div>
+// </section>
---
+++ src/components/NewsStories/NewsStoryCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./NewsStories/NewsStoryCard.css"
+
+const NewsStoryCard = () => {
+
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default NewsStoryCard;
