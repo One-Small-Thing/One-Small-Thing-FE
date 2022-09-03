dcb4e691bf28d9b8ed5f709ec98b748e98f7f557
Neil Linton <lintonbneil@gmail.com>
2022-09-03 12:42:56 -0700

    Add component

--- src/components/NewsStories/NewsStories.js
+++ src/components/NewsStories/NewsStories.js
@@ -9,38 +9,15 @@ const NewsStories = () => {
 
   if(loading) return "Loading..."
   if(error) return `Error! ${error.message}`
-  if(!loading){
-    const news = data.headlines.map(story => {
+    const headlinesArr = data.headlines
+    console.log(headlinesArr)
     
+  if(headlinesArr){
+    const news = headlinesArr.map(story => {
+
     return (
       <div className="story">
-        {!story.img ?
-        <section key={story.title} className="story__section" style={{ backgroundImage:`url(${peace})` }}
-                  alt="woman with making peace symbol with hand in air at a rally">
-            <div className="story__section__tag-box">
-              <p className="tags">EDUCATION {story.tags}</p>
-              <p className="action">TAKE ACTION</p>
-            </div>
-            <div className="story__section__title-box">
-              <h2 className="title">{story.title}</h2>
-              <p className="author-name">by Author Mc Authorson {story.author}</p>
-              <p className="date">{story.date}</p>
-            </div>
-          </section>
-          : 
-          <section key={story.title} className="story__section" style={{ backgroundImage:`url(${story.img})` }}
-                    alt="still image scene from corresponding news article">
-              <div className="story__section__tag-box">
-                <p className="tags">EDUCATION {story.tags}</p>
-                <p className="action">TAKE ACTION</p>
-              </div>
-              <div className="story__section__title-box">
-                <h2 className="title">{story.title}</h2>
-                <p className="author-name">by Author Mc Authorson {story.author}</p>
-                <p className="date">{story.date}</p>
-              </div>
-          </section>
-        }
+
       </div>
     )
     })
