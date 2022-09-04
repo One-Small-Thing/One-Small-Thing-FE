3ea2d519b2c1dddce3aa5d35d113b9a1482f452a
Neil Linton <lintonbneil@gmail.com>
2022-09-04 11:43:55 -0700

    Add small bug fix in Article with useEffect, state, and title for accessibility screen readers

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -9,12 +9,12 @@ function Article() {
 
     useEffect(() => {
         setEmbedUrl(location.state)
-    }, [embedUrl])
+    }, [location.state])
 
     
     return(
         <div className="Link__Ember">
-            <iframe type="External__Url" src={embedUrl} height="800px" width="800px"></iframe>
+            <iframe type="External__Url" title="News Article" src={embedUrl} height="800px" width="800px"></iframe>
         </div>
     )
 }
