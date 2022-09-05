2e48faf74deb044250089bd1f4bcc43e8ab73286
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-09-05 11:53:21 -0600

    remove image from nav bar and center title

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,10 +5,9 @@ import './NavBar.css';
 const NavBar = () => {
     return (
         <div className='navbar'>
-            <img className='navbar__img' src='https://cdn-icons-png.flaticon.com/512/1917/1917613.png'alt="pineapple"/>
             <div className='navbar__main'>
                 <h1 className='navbar__main--display'>One Small Thing</h1>
-                <div className='navbar__main--menu navbar__main--display'>
+                <div className='navbar__main--menu'>
                     <input type='checkbox' className='navbar__main--menu--checkbox' id='navi-toggle'/>
                     <label htmlFor='navi-toggle' className='navbar__main--menu--button'>
                         <span className='navbar__main--menu--icon'>&nbsp;</span>
@@ -19,8 +18,6 @@ const NavBar = () => {
                             <li className='navbar__main--menu--item'><div className='navbar__main--menu--link'>About</div></li>
                             <li className='navbar__main--menu--item'><div className='navbar__main--menu--link'>Main</div></li>
                             <li className='navbar__main--menu--item'><div className='navbar__main--menu--link'>About 3</div></li>
-                            <li className='navbar__main--menu--item'><div className='navbar__main--menu--link'>About 4</div></li>
-                            <li className='navbar__main--menu--item'><div className='navbar__main--menu--link'>About 5</div></li>
                         </ul>
                     </nav>
                 </div>
