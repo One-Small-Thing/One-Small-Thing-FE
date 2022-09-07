36c20db4fce590d716708539501902ce3b6a0d00
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-09-06 18:56:37 -0600

    Add team names to About section and link Github profiles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,12 +5,24 @@ const About = () => {
     return ( 
         <div>
             <article className="about-main">
-                <h1>About our application:</h1><br/><br/>
+                <h1>About our application:</h1><br/>
                 <p>"One Small Thing" is a capstone project made with love from students from Turing School of Software and Design. The idea came about from a sense of helplessness after the Uvalde shootings, and this project is dedicated to those 21 souls lost with a hope that the world can change for the better. <br/><br/>
-                There is so much happening in the world, that it can feel overwhelming at times. If only there was "one small thing" we could do to help the situation. Instead of just reading the news and feeling despair and sadness, what if we could take action on each soorty we read? <br/><br/>
+                There is so much happening in the world, that it can feel overwhelming at times. If only there was "one small thing" we could do to help the situation. Instead of just reading the news and feeling despair and sadness, what if we could take action on each story we read? <br/><br/>
                 This application allows users to see current events on a page, click on one of interest, and view what actionable things they can do to create change around them. Whether it's calling up a local politician to reform laws, writing letters of appreciation to front-line workers in hospitals, or simply meditating to stay grounded , "One Small Thing" is a place of solutions amongst a world of "what can I do to help". <br/><br/>
                 For more information on Makenna, Layla, Maranda, Nevaeh, Jose Manuel, Xavier, Tess, Ellie, Eliahna, Annabell, Jackie, Uziyah, Jayce, Maite, Jailah, Irma, Eva, Amerie, Lexi, Alithia, and their impact in the world, <a target="_blank" href="https://www.texastribune.org/2022/05/25/uvalde-school-shooting-victims/" className="external-link-uvalde" rel="noreferrer">click here.</a> <br/><br/>
                 </p>
+                <h3>BackEnd Team: 
+                    <p><a target="_blank" href="https://github.com/SaiHall" rel="noreferrer">Sai Hall, </a>
+                    <a target="_blank" href="https://github.com/stirlhoss" rel="noreferrer">Stirling Hostetter, </a>
+                    <a target="_blank" href="https://github.com/ColinReinhart" rel="noreferrer">Colin Reinhart, </a>
+                    <a target="_blank" href="https://github.com/Deming-Matt" rel="noreferrer">Matt Deming</a></p>
+                </h3>
+                <br/>
+                <h3>Front-End Team:
+                    <p><a target="_blank" href="https://github.com/jordan-sullivan" rel="noreferrer">Jordan Sullivan, </a>
+                    <a target="_blank" href="https://github.com/camianderson" rel="noreferrer">Cami Anderson, </a>
+                    <a target="_blank" href="https://github.com/Neil-B-Linton" rel="noreferrer">Neil Linton</a></p>
+                </h3>
             </article>
         </div>
      );
