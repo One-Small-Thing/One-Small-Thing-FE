1d11518cbdac34c6219c68286fdd462261f12bd8
Cami Anderson <camidantas@icloud.com>
2022-09-07 11:19:00 -0400

    Add Loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Loading.css';
+
+const Loading = () => {
+    return (
+        <div className="loading"> Loading... </div>
+    )
+}
+
+export default Loading;
