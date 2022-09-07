801a2c6088c1ac01b004131d313396dc03da1dd0
Cami Anderson <camidantas@icloud.com>
2022-09-07 15:01:46 -0400

    Add tests to homepage and NewsStories

--- cypress/e2e/Homepage_spec.cy.js
+++ cypress/e2e/Homepage_spec.cy.js
@@ -1,67 +1,68 @@
-describe('Homepage', () => {
+describe("Homepage", () => {
+  beforeEach("A user should see the homepage", () => {
+    cy.visit("http://localhost:3000/");
+  });
 
-  beforeEach('A user should see the homepage', () => {
-    cy.visit('http://localhost:3000/')
-  })
+  it("Should display a title to the user", () => {
+    cy.get(".navbar").within(() => {
+      cy.get("h1.navbar__main--display").should("be.visible");
+    });
+  });
 
-  it('Should display a title to the user', () => {
-    cy.get('.navbar').within(() => {
-      cy.get('h1.navbar__main--display').should('be.visible')
-    })
-  })
+  it("Should display a hamburger menu button", () => {
+    cy.get(".navbar").within(() => {
+      cy.get(".navbar__main--menu--button").should("be.visible");
+    });
+  });
 
-  it('Should display a hamburger menu button', () => {
-    cy.get('.navbar').within(() => {
-      cy.get('.navbar__main--menu--button').should('be.visible')
-    })
-  })
+  it("Should allow the user to open on click", () => {
+    cy.get(".navbar").within(() => {
+      cy.get(".navbar__main--menu--button").click();
+    });
+  });
 
-  it('Should allow the user to open on click', () => {
-    cy.get('.navbar').within(() => {
-      cy.get('.navbar__main--menu--button').click()
-    })
-  })
+  it("Should display two buttons to the user", () => {
+    cy.get(".navbar").within(() => {
+      cy.get(".navbar__main--menu--button").click();
+      cy.get(":nth-child(1) > .navbar__main--menu--link").should("be.visible");
+      cy.get(":nth-child(2) > .navbar__main--menu--link").should("be.visible");
+    });
+  });
 
-  it('Should display two buttons to the user', () => {
-    cy.get('.navbar').within(() => {
-      cy.get('.navbar__main--menu--button').click()
-      cy.get(':nth-child(1) > .navbar__main--menu--link').should('be.visible')
-      cy.get(':nth-child(2) > .navbar__main--menu--link').should('be.visible')
-    })
-  })
+  it("Should display a list of crisis lines in to the user at the bottom", () => {
+    cy.get(".navbar").within(() => {
+      cy.get(".navbar__main--menu--button").click();
+    });
+  });
 
-  it('Should display a list of crisis lines in to the user at the bottom', () => {
-    cy.get('.navbar').within(() => {
-      cy.get('.navbar__main--menu--button').click()
-    })
-  })
+  it("Should have the ability to redirect the user on click", () => {
+    cy.get(".navbar").within(() => {
+      cy.get(".navbar__main--menu--button").click();
+      cy.get(".navbar__main--menu--link").eq(0).click();
+      cy.url().should("eq", "http://localhost:3000/About");
+    });
+  });
 
-  it('Should have the ability to redirect the user on click', () => {
-    cy.get('.navbar').within(() => {
-      cy.get('.navbar__main--menu--button').click()
-      cy.get('.navbar__main--menu--link').eq(0).click()
-    })
-  })
+  it("Should close when a user clicks an option in the hamburger menu or when the x-button is clicked", () => {
+    cy.get(".navbar").within(() => {
+      cy.get(".navbar__main--menu--button").click();
+      cy.get(".navbar__main--menu--button").click();
+    });
+  });
 
-  it('Should close when a user clicks an option in the hamburger menu or when the x-button is clicked', () => {
-    cy.get('.navbar').within(() => {
-      cy.get('.navbar__main--menu--button').click()
-      cy.get('.navbar__main--menu--button').click()
-    })
-  })
+  it("Should display a little paragraph to the user about the page", () => {
+    cy.get(".hot-topics__desc > p").should("be.visible");
+  });
 
-  it('Should display a little paragraph to the user about the page', () => {
-    cy.get('.hot-topics__desc > p').should('be.visible')
-  })
+  it("Should allow the user to see 9 tiles of topics", () => {
+    cy.get('[href="/NewsStories/LGBTQ+"] > .topics__box').should("be.visible");
+    cy.get('[href="/NewsStories/LGBTQ+"] > .topics__box').contains("LGBTQ+");
+  });
 
-  it('Should allow the user to see 9 tiles of topics', () => {
-    cy.get('[href="/NewsStories/LGBTQ+"] > .topics__box').should('be.visible')
-    cy.get('[href="/NewsStories/LGBTQ+"] > .topics__box').contains('LGBTQ+')
-  })
-
-  it('Should allow the user to click on an option and be redirected to a page corresponding to the option clicked', () => {
-    cy.get('[href="/NewsStories/LGBTQ+"] > .topics__box').should('be.visible')
-    cy.get('[href="/NewsStories/LGBTQ+"] > .topics__box').contains('LGBTQ+')
-    cy.get('[href="/NewsStories/LGBTQ+"] > .topics__box').click()
-  })
-})
+  it("Should allow the user to click on an option and be redirected to a page corresponding to the option clicked", () => {
+    cy.get('[href="/NewsStories/LGBTQ+"] > .topics__box').should("be.visible");
+    cy.get('[href="/NewsStories/LGBTQ+"] > .topics__box').contains("LGBTQ+");
+    cy.get('[href="/NewsStories/LGBTQ+"] > .topics__box').click();
+    cy.url().should("eq", "http://localhost:3000/NewsStories/LGBTQ+");
+  });
+});
--- cypress/e2e/NewsStories.cy.js
+++ cypress/e2e/NewsStories.cy.js
@@ -1,44 +1,66 @@
-import { aliasQuery, hasOperationName } from "../utils/graphql-test-utils";
+describe("NewsStories component flow", () => {
+  beforeEach(() => {
+    cy.selectTopic();
+  });
 
-describe('NewsStories component flow', () => {  
-    it('Should display all the news selected by topic', () => {
-        cy.selectTopic();
-        cy.get('.story__section').should('exist');
-        cy.get('.story').children('.story__section').each(() => {
-            cy.get('.btn-ta')
-              .should('exist');
-            cy.get('.story-container__topic')
-              .should('exist');
-            cy.get('.title')
-              .should('exist');
-            cy.get('.author-name')
-              .should('exist');
-            cy.get('.date')
-              .should('exist');
-        });
-    })
+  it("Should display a title to the user", () => {
+    cy.get(".navbar").within(() => {
+      cy.get("h1.navbar__main--display").should("be.visible");
+    });
+  });
 
-    it('Should be able to go back to the homepage', () => {
-        cy.get('.navbar__main--display').click();
-        cy.url().should('eq', 'http://localhost:3000/');
-    })
+  it("Should be able to go back to the homepage", () => {
+    cy.get(".navbar__main--display").click();
+    cy.url().should("eq", "http://localhost:3000/");
+  });
 
-    it('Should be able to see the footer', () => {
-        cy.get('.footer').should('contain', 'Helpline');
-    })
+  it("Should display a hamburger menu button", () => {
+    cy.get(".navbar").within(() => {
+      cy.get(".navbar__main--menu--button").should("be.visible");
+    });
+  });
 
-    it('Should be able to click the Take Action button and see the CongressAction option', () => {
-        cy.selectTopic();
-        cy.get('.btn-ta').eq(0).click();
-        cy.get('.modal-window').should('exist');
-        cy.contains('.congress-action', 'Write to your local Congress member');
-        cy.contains('p', 'Take action by writing a letter or making a call to your local congress person about this issue!');
-    })
+  it("Should display all the news selected by topic", () => {
+    cy.get(".story__section").should("exist");
+    cy.get(".story")
+      .children(".story__section")
+      .each(() => {
+        cy.get(".btn-ta").should("exist");
+        cy.get(".story-container__topic").should("exist");
+        cy.get(".title").should("exist");
+        cy.get(".author-name").should("exist");
+        cy.get(".date").should("exist");
+      });
+  });
 
-    it('Should be able to click the CongressAction option inside the modal', () => {
-        cy.selectTopic();
-        cy.get('.btn-ta').eq(0).click();
-        cy.get('.congress-action').eq(0).click();
-        cy.url().should('eq', 'http://localhost:3000/CongressAction');
-    })
-})
+  it("Should be able to see the footer", () => {
+    cy.get(".footer").should("contain", "Helpline");
+  });
+
+  it("Should be able to click the Take Action button and see the CongressAction option", () => {
+    cy.get(".btn-ta").eq(0).click();
+    cy.get(".modal-window").should("exist");
+    cy.contains(".congress-action", "Write to your local Congress member");
+    cy.contains(
+      "p",
+      "Take action by writing a letter or making a call to your local congress person about this issue!"
+    );
+  });
+
+  it("Should be able to click the Take Action button and see the Meditation option", () => {
+    cy.contains(
+      ".external-link-meditation",
+      "Practice a 5 minute personal meditation to ease your stress."
+    );
+    cy.contains(
+      "p",
+      "You can take action just by resetting your mind and body. Take action by centering your thoughts and meditating."
+    );
+  });
+
+  it("Should be able to click the CongressAction option inside the modal", () => {
+    cy.get(".btn-ta").eq(0).click();
+    cy.get(".congress-action").eq(0).click();
+    cy.url().should("eq", "http://localhost:3000/CongressAction");
+  });
+});
