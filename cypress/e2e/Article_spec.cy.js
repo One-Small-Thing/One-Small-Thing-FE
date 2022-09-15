d0564ff8746428a35468a63ead1e3ee45419a93d
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-09-14 18:05:43 -0600

    Replace data with mock data

--- cypress/e2e/Article_spec.cy.js
+++ cypress/e2e/Article_spec.cy.js
@@ -1,12 +1,11 @@
 describe('Article', () => {
   beforeEach('A user should be on the News Article Page', () => {
-    cy.visit("http://localhost:3000/One-Small-Thing-FE/Article")
-    cy.get('[href="/NewsStories/Environment"] > .topics__box').click()
-    cy.get(':nth-child(1) > .story__section > .Link__Article > .story__section__title-box > .title').click()
+    cy.selectTopic()
+    cy.get('.title').eq(0).click()
   })
 
   it('Should store a unique article related to the users search', () => {
     cy.get('iframe').should('be.visible')
-    cy.url().should('eq', 'http://localhost:3000/One-Small-Thing-FE/Article/Peru%20to%20sue%20Repsol%20for%20$4.5bn%20over%20oil%20spill')
+    cy.url().should('eq', 'http://localhost:3000/One-Small-Thing-FE/Article/New%20PAC%20forms%20to%20target%20anti-LGBTQ%20candidates')
   })
 })
